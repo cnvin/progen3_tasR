f1f33a01d404562063dc1a9c6395d5451d6aba52
Sarthak Jain <165684262+sjain-profluent@users.noreply.github.com>
2025-07-20 18:55:35 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 pip install -e .
 pip install "megablocks[gg]==0.7.0" --no-build-isolation
-MAX_JOBS=4 pip install flash-attn --no-build-isolation
+MAX_JOBS=4 pip install flash-attn==2.7.4.post1 --no-build-isolation
